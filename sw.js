378ead07827483b258041ccb6133d812328a0b42
Travis CI User <travis@example.org>
2021-04-12 07:59:30 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-505f32369e20130eefa9.js"
+    "url": "webpack-runtime-0b76339a941f81c1ee9f.js"
   },
   {
     "url": "styles.fb54805cd13076860d45.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ce7141a5998fb3e2cfc9246325941fdc"
+    "revision": "c5bc6243edaac42bf26c1cf2f8295cf8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-1fb9d646ca17460098cf.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f79d8ee4e783265013a5c6345ffca5c1"
+    "revision": "182b3fbb61339e58e49299ecaa5e4e8a"
   },
   {
     "url": "polyfill-3e87bf22a321e5bacb05.js"
